d8eb65abd805edb6aae52f2af9a93238a15ed268
Carl Nathan <2540394@edu.vaniercollege.qc.ca>
2026-05-11 11:48:07 -0400

    Implement and fill borrowItem(Item item) method in Student class

--- src/main/java/org/carl/usermanagement/Student.java
+++ src/main/java/org/carl/usermanagement/Student.java
@@ -20,4 +20,26 @@ public class Student extends User {
         super(userId, name, borrowedItems, gender);
         this.borrowingLimit = Constants.MAX_BOOKS_STUDENT;
     }
+
+    /**
+     * method that borrows an item for a student. It has two conditions: it must
+     * be unique (different ids) and the item must be a book
+     * @param item the item to be borrowed
+     * @return the boolean value that shows the
+     */
+    @Override
+    public boolean borrowItem(Item item) {
+        if (item.getStatus() == Item.Status.IN_STORE) {
+            if (this.borrowedItems.contains(item) && !(item instanceof Book)) {
+                System.out.println("Item cannot be borrowed. Check if it is already in " +
+                        "your student's list, or if it is a book");
+                return false;
+            }
+            borrowedItems.add(item);
+            return true;
+        } else {
+            System.out.println("Item has already been borrowed");
+            return false;
+        }
+    }
 }
