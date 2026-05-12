183e138e329754a3a65a29c64a276a3dfa4158ca
Carl Nathan <2540394@edu.vaniercollege.qc.ca>
2026-05-12 00:14:29 -0400

    Implement and add generateReport() method in Admin

--- src/main/java/org/carl/usermanagement/Admin.java
+++ src/main/java/org/carl/usermanagement/Admin.java
@@ -4,4 +4,26 @@ import org.carl.other.Reportable;
 
 public class Admin implements Reportable {
 
+    @Override
+    public void generateReport(List<Item> items) {
+        System.out.println("LIBRARY STATUS REPORT\n");
+
+        System.out.println("BORROWED ITEMS:");
+        List<Item> borrowedItems = items.stream().
+                filter((item) -> item.getStatus() == Item.Status.BORROWED)
+                .toList();
+        System.out.println(borrowedItems);
+
+        System.out.println("\nIN STORE ITEMS:");
+        List<Item> inStoreItems = items.stream().
+                filter((item) -> item.getStatus() == Item.Status.IN_STORE)
+                .toList();
+        System.out.println(inStoreItems);
+
+        System.out.println("\nLOST ITEMS:");
+        List<Item> lostItems = items.stream().
+                filter((item) -> item.getStatus() == Item.Status.LOST)
+                .toList();
+        System.out.println(lostItems);
+    }
 }
