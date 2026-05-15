d3c81a28a372d65b24ee810c820ab45c0a7a7335
Carl Nathan <2540394@edu.vaniercollege.qc.ca>
2026-05-14 22:22:55 -0400

    Initialize the scanner for the user guide

--- src/main/java/org/carl/Main.java
+++ src/main/java/org/carl/Main.java
@@ -5,7 +5,27 @@ import org.carl.other.LibraryException;
 
 
 public class Main {
-    public static void main(String[] args) throws LibraryException {
+    static Library library = new Library();
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        library.loadData();
+
+        while (true) {
+            System.out.println("\n" + "WELCOME TO THE LIBRARY MANAGEMENT SYSTEM\n"
+            + "Please select an option: \n" +
+                    "1. Search | 2. Borrow | 3. Return | 4. Exit");
+            String choice = scanner.nextLine();
+
+            if (choice.equals("4")) break;
+
+            switch (choice) {
+                case "1" -> runSearch();
+                case "2" -> runBorrow();
+                case "3" -> runReturn();
+            }
+        }
+    }
 
         /*
         TESTING
